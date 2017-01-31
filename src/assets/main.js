2d63bf66c42e707ad430c6abfabd4798c6b61e03
Chris Neitzer <chris@neitzer.com>
2017-01-31 10:29:22 -0500

    Get codeschool badges through ajax request.
    Display badges return from codeschool.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
+  $.ajax({
+      url: "https://www.codeschool.com/users/aeonblue3.json"
+    , dataType: "jsonp"
+    , success: function(data) {
+        var html = '';
 
-  // your code will go here
-
+        data.courses.completed.forEach(function(course, i) {
+          html += "<div class=\"course\">\n" +
+                  "\t<h3>" + course.title + "</h3>\n" +
+                  "\t<img src=\"" + course.badge + "\">\n" +
+                  "\t<a href=\"" + course.url + "\" target=\"_blank\" class=\"btn btn-primary\">See Course<\a>\n" +
+                  "</div>\n";
+        }, this);
+        $("#badges").append($(html));
+      }
+  });
 });
